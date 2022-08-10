b5f1de64d7d2f07d38692f42d0442bcecd3d3905
John <johnmulvey48@yahoo.com>
2022-08-10 12:16:59 -0400

    git@github.com:Jmulvez/phase-3-ruby-oo-self-count-sentences-lab.git

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,15 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+    def report_on_sentence(value)
+      value.includes?(".")
+    end
   end
 
   def question?
-
+    def report_on_question(value)
+      value.includes?("?")
+    end
   end
 
   def exclamation?
-
+    def report_on_exclamation(value)
+      value.includes?("!")
+    end
   end
 
   def count_sentences
